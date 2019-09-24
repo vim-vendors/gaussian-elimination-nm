7ba4749a7bfd308e24511e39198491567a1baa8c
Jerry Fernandez <gmfernandez@cpp.edu>
2019-09-24 00:42:59 -0700

    Started Gauss w/SPP.

--- Assigment2.py
+++ Assigment2.py
@@ -9,24 +9,86 @@ b = np.array([10, 7, 15], dtype='float32')
 
 def NaiveGauss(aMatrix, bMatrix):
 	size = len(bMatrix)
-	subMatrix = np.zeros(size)
+	subVector = np.zeros(size)
+	# forward elimination
 	for k in range(0, size-1):
 		for i in range(k+1, size):
 			xMult = aMatrix[i, k] / aMatrix[k, k]
 			aMatrix[i, k] = xMult
 			for j in range(k+1, size):
-				aMatrix[i, j] = aMatrix[i, j] - (xMult * aMatrix[k, j])
-			bMatrix[i] = bMatrix[i] - (xMult * bMatrix[k])
-	subMatrix[size-1] = bMatrix[size-1] / aMatrix[size-1, size-1]
+				aMatrix[i, j] -= (xMult * aMatrix[k, j])
+			bMatrix[i] -= (xMult * bMatrix[k])
+	# back substitution
+	subVector[size-1] = bMatrix[size-1] / aMatrix[size-1, size-1]
 	for i in range(size-2, -1, -1):
 		_sum = bMatrix[i]
 		for j in range(i+1, size):
-			_sum = _sum - (aMatrix[i, j] * subMatrix[j])
-		subMatrix[i] = _sum / aMatrix[i, i]
-	#code to format the output, float32 produces weird results like -0.0
-	for x in range(len(subMatrix)):
-		if subMatrix[x] == -0.0:
-			subMatrix[x] = 0.0
-	return subMatrix
-
+			 _sum -= (aMatrix[i, j] * subVector[j])
+		subVector[i] = _sum / aMatrix[i, i]
+	# code to format the output, float32 produces weird results like -0.0
+	for x in range(len(subVector)):
+		if subVector[x] == -0.0:
+			subVector[x] = 0.0
+	return subVector
+#end NaiveGauss
+print("Pre Naive")
+print(a)
+print(b)
 print(NaiveGauss(a,b))
+print("Post Naive")
+print(a)
+print(b)
+
+def Gauss(size, aMatrix):
+	print("Pre - aMatrix: ")
+	print(aMatrix)
+	indexArray = np.zeros((size,), dtype=int)
+	ratioArray = np.zeros(size)
+	for i in range(0, size):
+		indexArray[i] = i
+		smax = 0.0
+		for j in range(0, size):
+			smax = max(smax, aMatrix[i, j])
+		ratioArray[i] = smax
+	for k in range(0, size-1):
+		rmax = 0
+		for i in range(k, size):
+			#pdb.set_trace()
+			r = abs(aMatrix[indexArray[i], k] / ratioArray[indexArray[i]])
+			if (r > rmax):	
+				rmax = r
+				j = i
+    	#Lj <--> Lk? Algorithm will not work without this
+		temp = indexArray[j]
+		indexArray[j] = indexArray[k]
+		indexArray[k] = temp
+		for i in range(k+1, size):
+			xMult = (aMatrix[indexArray[i], k] / aMatrix[indexArray[k], k])
+			aMatrix[indexArray[i], k] = xMult
+			for j in range(k+1, size):
+				aMatrix[indexArray[i], j] -= (xMult*aMatrix[indexArray[k], j])
+#need to return something?
+	print("Post - aMatrix: ")
+	print(aMatrix)
+	print("ratioArray:")
+	print(ratioArray)
+	print("indexArray: ")
+	print(indexArray)
+#end Gauss
+
+print("Calling Gauss w/SPP method: ")
+Gauss(len(b),a)
+
+def Solve(size, aMatrix, indexArray, bMatrix, subVector):
+	for k in range(0, size-1):
+		for i in range(k+1, n):
+			bMatrix[indexArray[i]] -= (aMatrix[indexArray[i], k] * bMatrix[indexArray[k]])
+	subVector[size-1] = ( bMatrix[indexArray[size-1]] / aMatrix[indexArray[size-1], size-1])
+	for i in range(size-2, -1, -1):
+		_sum = bMatrix[indexArray[i]]
+		for j in range(i+1, n):
+			_sum -= (aMatrix[indexArray[i], j] * subVector[j]) 	
+		subVector[i] = _sum / aMatrix[indexArray[i], i] 
+#need to return something?
+#need print statements
+#end Solve	
